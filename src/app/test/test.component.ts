57f9bb96857ce200a31638191a101c7a221d3053
Skelyos <pat123john123@gmail.com>
2018-03-19 11:36:44 +0000

    Created a new component

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AdjectiveService } from '../../service/adjective.service';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  UserInput: string;
+
+  constructor(private adjectiveService: AdjectiveService) { }
+
+  ngOnInit() {
+  }
+
+  searchAdjective() {
+    this.adjectiveService.getFirstLetter(this.UserInput);
+    debugger;
+  }
+}
