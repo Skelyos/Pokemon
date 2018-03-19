a56341e3a1e37150aace207f89c759d11858bd54
Skelyos <pat123john123@gmail.com>
2018-03-19 11:37:22 +0000

    Other changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {TestComponent} from '../app/test/test.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AdjectiveService } from '../service/adjective.service';
+import { TestComponent } from './test/test.component';
+
+const appRoutes: Routes = [
+  { path: '', component: TestComponent },
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [AdjectiveService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
